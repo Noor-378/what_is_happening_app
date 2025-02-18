ba52c1dfba0a8beea37b2491a902de8ed6cdd2a4
Noor-378 <nooraldenmoh378@gmail.com>
2025-02-18 16:22:03 +0300

    create ListView and custom divider

--- lib/modules/business/business.dart
+++ lib/modules/business/business.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:what_is_happening_app/shared/components/build_article_item.dart';
+import 'package:what_is_happening_app/shared/components/custom_divider.dart';
+import 'package:what_is_happening_app/shared/cubit/cubit.dart';
+import 'package:what_is_happening_app/shared/cubit/states.dart';
 
 class BusinessScreen extends StatelessWidget {
   const BusinessScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const BuildArticleItem();
+    return BlocConsumer<AppCubit, AppStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return state is! GetBusinessLoadingState
+            ? ListView.separated(
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) => const BuildArticleItem(),
+                separatorBuilder: (context, index) => const CustomDivider(),
+                itemCount: 50,
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              );
+      },
+    );
   }
 }
