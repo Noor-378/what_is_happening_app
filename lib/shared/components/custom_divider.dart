a6b432fff263064804a877a84a94f9e5d4a1608c
Noor-378 <nooraldenmoh378@gmail.com>
2025-02-19 16:31:02 +0300

    change the height for the custom divider

--- lib/shared/components/custom_divider.dart
+++ lib/shared/components/custom_divider.dart
@@ -6,6 +6,7 @@ class CustomDivider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Divider(
+      height: 10,
       color: Colors.blueGrey,
       indent: 20,
       thickness: .3,
